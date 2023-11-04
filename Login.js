29fe9b6fd0e188f49fc49f13e9a68c3330c1587e
Omar0147 <71673879+Omar0147@users.noreply.github.com>
2023-11-04 15:26:54 -0400

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Here you would handle the login logic, possibly sending a request to your backend
+    console.log('Login with:', email, password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder="Email" 
+          required 
+        />
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Password" 
+          required 
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
